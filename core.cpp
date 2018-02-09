77c65ebe9b1352ddc7e079396b168db15b6aa505
Tsingtiry <xhj14012+git@gmail.com>
2018-02-09 10:35:47 +0800

    gao3

--- core.cpp
+++ core.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define _ ios_a::sync_with_stdio(0);cin.tie(0);
 #define QH freopen("qh.in", "r", stdin);
 #define SS ios::sync_with_stdio(false);
 #define LL long long
@@ -10,7 +10,7 @@
 #define MP(x,y) make_pair(x,y)
 #define MAX(a,b) a=max(a,b)
 #define MIN(a,b) a=min(a,b)
-#define mp make_pair
+//#define mp make_pair
 #define pb push_back
 #define gi(a) scanf("%d",&a);
 #define gi2(a,b) scanf("%d%d",&a,&b);
@@ -25,13 +25,23 @@
 #define CLINF(a) memset(a, 0x3f3f3f3f, sizeof(a))
 #define PII pair <int, int>
 #define PIII pair <int, PII>
+#define PIIII pair <int,PIII>
 using namespace std;
 char title[100];
 const int minlen = 685;
 const int maxlen = 695;
 #define fi first
 #define se second.first
-#define th second.second
+//#define th second.second
+
+#define third second.second.first
+#define forth second.second.second
+// PIII mpiii(int a, int b, int c) {
+// 	return mp(a, mp(b, c));
+// }
+PIIII mp(int a, int b, int c, int d = 0) {
+	return make_pair(a, make_pair(b, make_pair(c, d)));
+}
 int cnt = 0;
 int tot;
 vector<string> factory_cant_mix;
@@ -45,7 +55,7 @@ struct Part {
 		quality = (quality_s == "normal") ? 0 : 1;
 		cin >> length;
 		cin >> matrix;
-		index = 0;
+		group = 0;
 		//output();
 	}
 	void output() {
@@ -56,7 +66,7 @@ struct Part {
 		cout << ((quality == 0) ? "normal  " : "impurity") << "\t";
 		cout << length << "\t";
 		cout << matrix << "\t";
-		cout << index << endl;
+		cout << group << endl;
 	}
 	int id;//硅棒序号
 	string factory;
@@ -65,7 +75,7 @@ struct Part {
 	int quality;
 	int length;
 	int matrix;
-	int index;//拼接方案组号
+	int group;//拼接方案组号
 	// bool  operator < (const Part &a)const {
 	// 	return length < a.length;
 	// }
@@ -74,14 +84,13 @@ struct Part {
 bool cmp_length(const Part a, const Part b) {
 	return a.length < b.length;
 }
-bool cmp_index(const Part a, const Part b) {
-	return a.index < b.index;
+bool cmp_group(const Part a, const Part b) {
+	return a.group < b.group;
 }
 bool cmp_id(const Part a, const Part b) {
 	return a.id < b.id;
 }
-vector <Part> base;
-vector <Part> a, b, c;
+
 
 int check(int val) {
 	if (val < minlen) return -1;
@@ -89,29 +98,39 @@ int check(int val) {
 	//else if (val > maxlen)
 	return 0;
 }
-PIII mpiii(int a, int b, int c) {
-	return mp(a, mp(b, c));
-}
-vector<PIII> comb3;
-void comb3_erase(int p) {
-	for (std::vector<PIII>::iterator it = comb3.begin(); it != comb3.end();) {
-		if (it->se == p || it->th == p) {
-			it = comb3.erase(it);
-		} else {
-			++it;
-		}
-	}
-}
-vector<Part> calc3(vector<Part> v, bool if_cut = 0) {
+
+
+// vector<PIII> comb;
+// void comb_erase(int p) {
+// 	for (std::vector<PIII>::iterator it = comb.begin(); it != comb.end();) {
+// 		if (it->se == p || it->th == p) {
+// 			it = comb.erase(it);
+// 		} else {
+// 			++it;
+// 		}
+// 	}
+// }
+// vector<PIII> comb4;
+// void comb4_erase(int p) {
+// 	for (std::vector<PIII>::iterator it = comb4.begin(); it != comb4.end();) {
+// 		if (it->se == p || it->th == p) {
+// 			it = comb4.erase(it);
+// 		} else {
+// 			++it;
+// 		}
+// 	}
+// }
+vector<Part> calc4(vector<Part> v, bool if_cut = 1) {
+
 	vector<Part> result;
 	result.clear();
 	for (std::vector<Part>::iterator it = v.begin(); it != v.end();) {
-		if (it->index > 0) {
+		if (it->group > 0) {
 			result.pb(*it);
 			it = v.erase(it);
 		} else {
-			if (it->index == -1) {
-				it->index = 0;
+			if (it->group == -1) {
+				it->group = 0;
 			}
 			++it;
 		}
@@ -120,94 +139,228 @@ vector<Part> calc3(vector<Part> v, bool if_cut = 0) {
 	// for (std::vector<Part>::iterator it = v.begin(); it != v.end(); ++it) {
 	// 	it->output();
 	// }
-	//vector<PIII> comb3;
-	comb3.clear();
+	//vector<PIII> comb;
+	vector<PIIII> comb;
+	comb.clear();
 	int size = v.size();
-	for (int i = 1; i < size; ++i) { //组对(i+j,i,j)
-		for (int j = 0; j < i; ++j) {
-			comb3.pb(mpiii(v[i].length + v[j].length, i, j));
+	for (int i = 2; i < size; ++i) { //组对(i+j+k,i,j,k)
+		for (int j = 1; j < i; ++j) {
+			for (int k = 0; k < j; ++k) {
+				comb.pb(mp(v[i].length + v[j].length + v[k].length, i, j, k));
+			}
 		}
 	}
-	sort(comb3.begin(), comb3.end());
-	//reverse(comb3.begin(),comb3.end());
+	sort(comb.begin(), comb.end());
+	//reverse(comb.begin(),comb.end());
 	for (int p = v.size() - 1; p >= 0; --p) {
-		//if (comb3.size() == 0) break;
-		//comb3_erase(p);
-		if (v[p].index != 0) {
+		//if (comb.size() == 0) break;
+		//comb_erase(p);
+		if (v[p].group != 0) {
 			continue;
 			//result.pb(v[p]);
-			//comb3_erase(p);
+			//comb_erase(p);
 		} else {
-			vector<PIII>::iterator mit, it_low, it_upper;
-			it_low = lower_bound(comb3.begin(), comb3.end(), mpiii(minlen - v[p].length, 0, 0));
-			it_upper = upper_bound(comb3.begin(), comb3.end(), mpiii(maxlen - v[p].length + 1, 0, 0));
+			vector<PIIII>::iterator mit, it_low, it_upper;
+			it_low = lower_bound(comb.begin(), comb.end(), mp(minlen - v[p].length, 0, 0, 0));
+			it_upper = upper_bound(comb.begin(), comb.end(), mp(maxlen - v[p].length + 1, 0, 0, 0));
 			--it_upper;
-			mit = comb3.end();
-			if (it_low != comb3.end()) {
-				for (std::vector<PIII>::iterator it = it_low; it!=it_upper&&it != comb3.end(); ++it) {
-					if (v[it->se].index == 0 && v[it->th].index == 0) {
-						if (mit == comb3.end() || it->se < mit->se) { //启发式剪枝，调优点
+			mit = comb.end();
+			if (it_low != comb.end()) {
+				for (std::vector<PIIII>::iterator it = it_low; it != it_upper && it != comb.end(); ++it) {
+					if (v[it->se].group == 0 && v[it->third].group == 0 && v[it->forth].group == 0) {
+						if (mit == comb.end() || (it->se < mit->se && check(mit->fi + v[p].length) != 1)) { //启发式剪枝，调优点
 							mit = it;
 						}
 					}
 				}
 			}
 			if (check(mit->fi + v[p].length) != 1) {
-				mit = comb3.end();
+				mit = comb.end();
 			}
-			if (mit == comb3.end()) {
-				v[p].index = -1;
+			if (mit == comb.end()) {
+				v[p].group = -1;
 				//result.pb(v[p]);
-				//comb3_erase(p);
+				//comb_erase(p);
 			} else {
 				cnt++;
-				v[p].index = cnt;
-				v[mit->se].index = cnt;
-				v[mit->th].index = cnt;
+				v[p].group = cnt;
+				v[mit->se].group = cnt;
+				v[mit->third].group = cnt;
+				v[mit->forth].group = cnt;
 				// result.pb(v[p]);
 				// result.pb(v[mit->se]);
 				// result.pb(v[mit->th]);
 				//int t1 = mit->se;
 				//int t2 = mit->th;
-				//comb3_erase(p);
-				//comb3_erase(mit->se);//坑！
-				//comb3_erase(mit->th);
-				//comb3_erase(t1);
-				//comb3_erase(t2);
+				//comb_erase(p);
+				//comb_erase(mit->se);//坑！
+				//comb_erase(mit->th);
+				//comb_erase(t1);
+				//comb_erase(t2);
 			}
 		}
 	}
-	//it[1] = lower_bound(comb3.begin(), comb3.end(), mpiii(1, 0, 0));
-	// for (int i = 0; i < comb3.size(); ++i) {
-	// 	//printf("%d %d %d\n", comb3[i].fi,comb3[i].se,comb3[i].th);
+	//it[1] = lower_bound(comb.begin(), comb.end(), mpiii(1, 0, 0));
+	// for (int i = 0; i < comb.size(); ++i) {
+	// 	//printf("%d %d %d\n", comb[i].fi,comb[i].se,comb[i].th);
 	// }
 
 	for (int p = 0; p < size; ++p) {
 		result.pb(v[p]);
 	}
-	printf("result:\n");
-	sort(result.begin(), result.end(), cmp_index);
-	for (int i = 0; i < result.size(); ++i) {
-		//cout << i << " ";
-		result[i].output();
+	// printf("result:\n");
+	// sort(result.begin(), result.end(), cmp_group);
+	// for (int i = 0; i < result.size(); ++i) {
+	// 	//cout << i << " ";
+	// 	result[i].output();
+	// }
+	return result;
+}
+vector<Part> calc3(vector<Part> v, bool if_cut = 0) {
+
+	vector<Part> result;
+	result.clear();
+	for (std::vector<Part>::iterator it = v.begin(); it != v.end();) {
+		if (it->group > 0) {
+			result.pb(*it);
+			it = v.erase(it);
+		} else {
+			if (it->group == -1) {
+				it->group = 0;
+			}
+			++it;
+		}
 	}
+	sort(v.begin(), v.end(), cmp_length);
+	// for (std::vector<Part>::iterator it = v.begin(); it != v.end(); ++it) {
+	// 	it->output();
+	// }
+	//vector<PIII> comb;
+	vector<PIIII> comb;
+	comb.clear();
+	int size = v.size();
+	for (int i = 1; i < size; ++i) { //组对(i+j,i,j)
+		for (int j = 0; j < i; ++j) {
+			comb.pb(mp(v[i].length + v[j].length, i, j));
+		}
+	}
+	sort(comb.begin(), comb.end());
+	//reverse(comb.begin(),comb.end());
+	for (int p = v.size() - 1; p >= 0; --p) {
+		//if (comb.size() == 0) break;
+		//comb_erase(p);
+		if (v[p].group != 0) {
+			continue;
+			//result.pb(v[p]);
+			//comb_erase(p);
+		} else {
+			vector<PIIII>::iterator mit, it_low, it_upper;
+			it_low = lower_bound(comb.begin(), comb.end(), mp(minlen - v[p].length, 0, 0));
+			it_upper = upper_bound(comb.begin(), comb.end(), mp(maxlen - v[p].length + 1, 0, 0));
+			--it_upper;
+			mit = comb.end();
+			if (it_low != comb.end()) {
+				for (std::vector<PIIII>::iterator it = it_low; it != it_upper && it != comb.end(); ++it) {
+					if (v[it->se].group == 0 && v[it->third].group == 0) {
+						if (mit == comb.end() || (it->se < mit->se && check(it->fi + v[p].length) == 1)) { //启发式剪枝，调优点
+							mit = it;
+						}
+					}
+				}
+			}
+			if (check(mit->fi + v[p].length) != 1) {
+				mit = comb.end();
+			}
+			if (mit == comb.end()) {
+				v[p].group = -1;
+				//result.pb(v[p]);
+				//comb_erase(p);
+			} else {
+				cnt++;
+				v[p].group = cnt;
+				v[mit->se].group = cnt;
+				v[mit->third].group = cnt;
+				// result.pb(v[p]);
+				// result.pb(v[mit->se]);
+				// result.pb(v[mit->th]);
+				//int t1 = mit->se;
+				//int t2 = mit->th;
+				//comb_erase(p);
+				//comb_erase(mit->se);//坑！
+				//comb_erase(mit->th);
+				//comb_erase(t1);
+				//comb_erase(t2);
+			}
+		}
+	}
+	//it[1] = lower_bound(comb.begin(), comb.end(), mpiii(1, 0, 0));
+	// for (int i = 0; i < comb.size(); ++i) {
+	// 	//printf("%d %d %d\n", comb[i].fi,comb[i].se,comb[i].th);
+	// }
+
+	for (int p = 0; p < size; ++p) {
+		result.pb(v[p]);
+	}
+	// printf("result:\n");
+	// sort(result.begin(), result.end(), cmp_group);
+	// for (int i = 0; i < result.size(); ++i) {
+	// 	//cout << i << " ";
+	// 	result[i].output();
+	// }
 	return result;
 }
 
 void gao() {
 	//fgets(title,99,stdin);
 	cin.getline(title, 99);
-	base.clear();
+	vector <Part> a;
+	a.clear();
 	while (!cin.eof()) {
 		//for (int i = 0; i < 540; ++i) {
 		Part t;
 		t.input();
-		base.push_back(t);
+		a.push_back(t);
 	}
 	cnt = 0;
 	tot = 0;
-	base = calc3(base);
-	//base=calc4(base);
+	a = calc3(a);
+	// printf("/******report calc3******\n");
+	// sort(a.begin(), a.end(), cmp_group);
+	// //a = a;
+	// reverse(a.begin(), a.end());
+	// for (int i = 0, g = 0; i <= a.size(); ++i) {
+	// 	if (a[i].group <= 0) continue;
+	// 	g++;
+	// 	printf("group%d:\n", g);
+	// 	int sum = a[i].length;
+	// 	a[i].output();
+	// 	while (a[i].group == a[i + 1].group) {
+	// 		sum += a[i + 1].length;
+	// 		a[i + 1].output();
+	// 		++i;
+	// 	}
+	// 	printf("sum=%d\n", sum);
+	// }
+	// printf("/******report calc3******\n");
+	a = calc4(a);
+	printf("/******report calc4******\n");
+	sort(a.begin(), a.end(), cmp_group);
+	//a = a;
+	reverse(a.begin(), a.end());
+	for (int i = 0, g = 0; i <= a.size(); ++i) {
+		if (a[i].group <= 0) continue;
+		g++;
+		printf("group%d:\n", g);
+		int sum = a[i].length;
+		a[i].output();
+		while (a[i].group == a[i + 1].group) {
+			sum += a[i + 1].length;
+			a[i + 1].output();
+			++i;
+		}
+		printf("sum=%d\n", sum);
+	}
+	printf("/******report calc4******\n");
 }
 
 int main(int argc, char const *argv[]) {
